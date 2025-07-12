4ab76de16d778590c3324528bf802e06e1995003
Your Name <rozvasquez03@gmail.com>
2025-07-12 19:21:36 +0800

    remove shipping

--- src/components/Leverage.js
+++ src/components/Leverage.js
@@ -36,8 +36,8 @@ function Leverage() {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
         </svg>
       ),
-      title: "24/7 Support",
-      desc: "Always here to help"
+      title: "Fast Response",
+      desc: "Quick response to your messages"
     },
     {
       icon: (
@@ -66,15 +66,7 @@ function Leverage() {
       title: "Custom Designs",
       desc: "Personalized to perfection"
     },
-    {
-      icon: (
-        <svg className="w-8 h-8 text-pink-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      ),
-      title: "Worldwide Shipping",
-      desc: "Delivered anywhere"
-    }
+    
   ];
 
   // Duplicate features for seamless looping
